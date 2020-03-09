68d92cd37f8617dd199b4bc19893414b55e67a69
Laura Ingulsrud <lgi@mac.com>
2020-03-09 10:50:22 -0700

    update streampower package

--- R/stream_power.R
+++ R/stream_power.R
@@ -1,7 +1,7 @@
 #' Stream Power
 #'
 #' This function determines the power of a stream
-#' 
+#'
 #' @param Q stream discharge (m^3/s)
 #' @param S channel slope
 #' @param rho water density (g/m^3) default = 1000
@@ -11,7 +11,7 @@
 
 # Create stream power function
 stream_power = function(Q, S, rho=1000, g=9.8) {
-  # if inputs are negative, return message "[input] must be greater than zero" using two different kinds of error checking 
+  # if inputs are negative, return message "[input] must be greater than zero" using two different kinds of error checking
   Q = ifelse((Q < 0), return("discharge must be greater than zero"), Q)
   S = ifelse((S < 0), return("channel slope must be greater than zero"), S)
   if (rho < 0) return("rho must be greater than zero")
@@ -19,3 +19,4 @@ stream_power = function(Q, S, rho=1000, g=9.8) {
   result = rho * g * Q * S
   return(result)
 }
+
